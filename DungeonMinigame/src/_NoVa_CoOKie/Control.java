f9d42e7ef9be8c351338a61b038836a5bd73ed90
matthewjohnbennett <mben2010@live.ca>
2020-03-21 01:10:00 -0400

    Adding Initial Files.

---
+++ DungeonMinigame/src/_NoVa_CoOKie/Control.java
@@ -0,0 +1,14 @@
+package _NoVa_CoOKie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author _NoVa_CoOKie
+ */
+public class Control extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("Enabled DungeonMinigame Plugin!");
+	}
+}
